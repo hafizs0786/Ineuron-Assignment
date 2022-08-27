630326d728bdb5f5f5d35e9ebca10c894b370118
hafizs0786 <38181887+hafizs0786@users.noreply.github.com>
2022-08-27 18:53:36 +0530

    Add files via upload

---
+++ Assignment 2/Q9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{   
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("Size of int type variable is %d bytes\n",sizeof(a));
+    printf("Size of float type variable is %d bytes\n",sizeof(b));
+    printf("Size of char type variable is %d byte\n",sizeof(c));
+    printf("Size of double type variable is %d bytes\n",sizeof(d));
+    return 0;    
+}
